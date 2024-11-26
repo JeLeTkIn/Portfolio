24c3a8b0e40da8c5eb1844564c34018700526b57
JeLeTkIn <nurlan.wff@gmail.com>
2024-11-26 22:31:36 +0600

    fixes2

--- src/Pages/projects.js
+++ src/Pages/projects.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   const [index, setIndex] = useState(0); // Индекс текущего проекта
@@ -38,7 +39,7 @@ const Projects = () => {
             target="_blank" 
             rel="noopener noreferrer"
           >
-            <img src={project.image} alt={`Проект ${i + 1}`} />
+            <img src={project.image} alt={`project ${i + 1}`} />
             <p>{project.description}</p>
           </Link>
         ))}
